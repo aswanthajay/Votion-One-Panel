19770d98a2ee19a37d73f6277afdaaf05f48b9a5
aswanthajay <aswanthajay@users.noreply.github.com>
2026-08-24 06:48:07 +0530

    fix: expose functional audit log navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -34,10 +34,17 @@ export const Sidebar: React.FC<SidebarProps> = ({
         </svg>
       )
     },
-
-
+    {
+      title: 'Cluster Audit Logs',
+      key: 'audit-logs',
+      icon: (
+        <svg aria-hidden="true" height="16" viewBox="0 0 22 22" width="16" fill="currentColor">
+          <path clipRule="evenodd" d="M4 2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V7.414a2 2 0 0 0-.586-1.414l-4-4A2 2 0 0 0 14.586 2H4Zm11 1.414L18.586 7H15V3.414ZM13 3H4v16h14V9h-4a1 1 0 0 1-1-1V3Zm-7 7a1 1 0 0 1 1-1h8a1 1 0 0 1 1 2H7a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2H7Zm-1 5a1 1 0 0 1 1-1h5a1 1 0 1 1 0 0 2H7a1 1 0 0 1-1-1Z" fillRule="evenodd"></path>
+        </svg>
+      )
+    },
     { 
-      title: 'Cluster Connections', 
+      title: 'Cluster Connections',
       key: 'proxmox-connections',
       icon: (
         <svg aria-hidden="true" height="16" viewBox="0 0 22 22" width="16" fill="currentColor">
